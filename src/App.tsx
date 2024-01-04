ef2beef9a734e1f0d154adb6ad2750394bf29c06
doranakan <orlandi.m88@gmail.com>
2024-01-04 15:19:29 +0100

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,124 @@
+import {
+  Authenticated,
+  ErrorComponent,
+  Refine,
+  WelcomePage,
+} from '@refinedev/core';
+import { RefineKbar, RefineKbarProvider } from '@refinedev/kbar';
+import routerBindings, {
+  CatchAllNavigate,
+  DocumentTitleHandler,
+  NavigateToResource,
+  UnsavedChangesNotifier,
+} from '@refinedev/react-router-v6';
+import { dataProvider, liveProvider } from '@refinedev/supabase';
+import { BrowserRouter, Outlet, Route, Routes } from 'react-router-dom';
+import {
+  notificationProvider,
+  RefineThemes,
+  ThemedLayoutV2,
+} from '@refinedev/chakra-ui';
+
+import { ChakraProvider } from '@chakra-ui/react';
+import authProvider from './authProvider';
+import { supabaseClient } from './utility';
+import { LoginPage } from './pages';
+
+import { ChakraUIInferencer } from '@refinedev/inferencer/chakra-ui';
+
+function App() {
+  return (
+    <BrowserRouter>
+      <RefineKbarProvider>
+        {/* You can change the theme colors here. example: theme={RefineThemes.Magenta} */}
+        <ChakraProvider theme={RefineThemes.Blue}>
+          <Refine
+            authProvider={authProvider}
+            dataProvider={dataProvider(supabaseClient)}
+            liveProvider={liveProvider(supabaseClient)}
+            notificationProvider={notificationProvider}
+            options={{
+              syncWithLocation: true,
+              warnWhenUnsavedChanges: true,
+              projectId: 'QwIWik-v6bZYI-AdQS2F',
+            }}
+            resources={[
+              {
+                create: '/codexes/create',
+                edit: '/codexes/edit/:id',
+                list: '/codexes',
+                name: 'codexes',
+                show: '/codexes/show/:id',
+              },
+              {
+                create: '/factions/create',
+                edit: '/factions/edit/:id',
+                list: '/factions',
+                name: 'factions',
+                show: '/factions/show/:id',
+              },
+              {
+                create: '/units/create',
+                edit: '/units/edit/:id',
+                list: '/units',
+                name: 'units',
+                show: '/units/show/:id',
+              },
+            ]}
+            routerProvider={routerBindings}
+          >
+            <Routes>
+              <Route
+                element={
+                  <Authenticated fallback={<CatchAllNavigate to="/login" />}>
+                    <Outlet />
+                  </Authenticated>
+                }
+              >
+                <Route
+                  element={
+                    <ThemedLayoutV2>
+                      <Outlet />
+                    </ThemedLayoutV2>
+                  }
+                >
+                  <Route
+                    index
+                    element={<NavigateToResource resource="codexes" />}
+                  />
+                  <Route path="codexes">
+                    <Route index element={<ChakraUIInferencer />} />
+                    <Route path="show/:id" element={<ChakraUIInferencer />} />
+                    <Route path="edit/:id" element={<ChakraUIInferencer />} />
+                    <Route path="create" element={<ChakraUIInferencer />} />
+                  </Route>
+                  <Route path="factions">
+                    <Route index element={<ChakraUIInferencer />} />
+                    <Route path="show/:id" element={<ChakraUIInferencer />} />
+                    <Route path="edit/:id" element={<ChakraUIInferencer />} />
+                    <Route path="create" element={<ChakraUIInferencer />} />
+                  </Route>
+                  <Route path="units">
+                    <Route index element={<ChakraUIInferencer />} />
+                    <Route path="show/:id" element={<ChakraUIInferencer />} />
+                    <Route path="edit/:id" element={<ChakraUIInferencer />} />
+                    <Route path="create" element={<ChakraUIInferencer />} />
+                  </Route>
+                  <Route path="*" element={<ErrorComponent />} />
+                </Route>
+              </Route>
+              <Route element={<Authenticated fallback={<Outlet />} />}>
+                <Route path="/login" element={<LoginPage />} />
+              </Route>
+            </Routes>
+            <RefineKbar />
+            <UnsavedChangesNotifier />
+            <DocumentTitleHandler />
+          </Refine>
+        </ChakraProvider>
+      </RefineKbarProvider>
+    </BrowserRouter>
+  );
+}
+
+export default App;
---
+++ src/pages/LoginPage/LoginPage.tsx
@@ -0,0 +1,68 @@
+import {
+  Box,
+  Stack,
+  FormControl,
+  FormLabel,
+  Input,
+  Button,
+} from '@chakra-ui/react';
+import { useLogin } from '@refinedev/core';
+import { FormikConfig, useFormik } from 'formik';
+import { useCallback } from 'react';
+
+type Credentials = { email: string; password: string };
+
+const LoginPage: React.FC = () => {
+  const { mutate: login } = useLogin();
+
+  const handleLogin = useCallback<FormikConfig<Credentials>['onSubmit']>(
+    (values) => {
+      login(values);
+    },
+    [login]
+  );
+
+  const { handleChange, handleSubmit, values } = useFormik<Credentials>({
+    initialValues: {
+      email: '',
+      password: '',
+    },
+    onSubmit: handleLogin,
+  });
+
+  return (
+    <Box
+      borderRadius={8}
+      borderWidth={1}
+      boxShadow="lg"
+      maxWidth="md"
+      mt={100}
+      mx="auto"
+      p={8}
+    >
+      <Stack spacing={4}>
+        <FormControl id="email">
+          <FormLabel>Email address</FormLabel>
+          <Input
+            type="email"
+            value={values.email}
+            onChange={handleChange('email')}
+          />
+        </FormControl>
+        <FormControl id="password">
+          <FormLabel>Password</FormLabel>
+          <Input
+            type="password"
+            value={values.password}
+            onChange={handleChange('password')}
+          />
+        </FormControl>
+        <Button colorScheme="blue" onClick={() => handleSubmit()}>
+          Sign In
+        </Button>
+      </Stack>
+    </Box>
+  );
+};
+
+export default LoginPage;
