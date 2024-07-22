15c02850b90e827723df3519d4f089d062143dfa
doranakan <orlandi.m88@gmail.com>
2024-07-22 18:04:42 +0200

    feat: title

--- src/utility/supabaseClient.ts
+++ src/utility/supabaseClient.ts
@@ -1,8 +1,7 @@
 import { createClient } from '@refinedev/supabase'
 
-const SUPABASE_URL = 'http://localhost:54321'
-const SUPABASE_KEY =
-  'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZS1kZW1vIiwicm9sZSI6ImFub24iLCJleHAiOjE5ODM4MTI5OTZ9.CRXP1A7WOeoJeXxjNni43kdQwgnWNReilDMblYTn_I0'
+const SUPABASE_URL = import.meta.env.VITE_SUPABASE_URL
+const SUPABASE_KEY = import.meta.env.VITE_SUPABASE_ANON_KEY
 
 export const supabaseClient = createClient(SUPABASE_URL, SUPABASE_KEY, {
   auth: {
